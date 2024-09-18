6fac9bbacf970c17637485f16aa0e2c343263b9c
Bisra Kebere <kiyyakebere@gmail.com>
2024-09-18 22:09:41 +0300

    feat: Update database

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ module.exports = {
   env: {
     // HOST
     HOST_API_KEY: '',
-    // DB
-    MONGODB_URI:
-      process.env.MONGODB_URI ||
-      'mongodb+srv://beteliq:RlQCd4IEn7JdrLQN@cluster0.7p9heea.mongodb.net/?retryWrites=true&w=majority',
     // CLOUD
     REACT_APP_CLOUD_NAME: 'dbyn25cyp',
     REACT_APP_CLOUD_API_KEY: '785511957617838',
